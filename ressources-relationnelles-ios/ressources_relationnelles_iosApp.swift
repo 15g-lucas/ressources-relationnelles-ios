263417ea83084982f7b84ebfbce9372f5d0b1dd7
arthurbret <abret.56@gmail.com>
2025-04-11 13:00:34 +0200

    Initial Commit

---
+++ ressources-relationnelles-ios/ressources_relationnelles_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ressources_relationnelles_iosApp.swift
+//  ressources-relationnelles-ios
+//
+//  Created by Arthur BRET on 11/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ressources_relationnelles_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
